77e4dcdbe7a7e3e4dd141e82b88edabdc77ed5a4
joaotsuchiya <joao.tsuchiya@db1.com.br>
2025-11-25 23:05:13 -0300

    Configuração

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['antd'],
+  images: {
+    domains: [],
+  },
+};
+
+module.exports = nextConfig;
